e1d2b133041225f26c9b283d1c566450cab27810
kyle <kyle-murphy.co.uk>
2018-04-11 16:14:14 +0100

    versioning issue

--- reseterafilter.user.js
+++ reseterafilter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Resetera filter threads
-// @version      0.61
+// @version      0.7
 // @description  Filters threads based on keywords
 // @author       Kyle Murphy
 // @match        https://www.resetera.com/*
