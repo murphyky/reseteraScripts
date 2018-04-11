96f76d0fa58bc37f89ad3c298dd126436e1907fd
kyle <kyle-murphy.co.uk>
2018-04-11 12:32:29 +0100

    fix config

--- reseterafilter.user.js
+++ reseterafilter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Resetera filter threads
-// @version      0.2
+// @version      0.3
 // @description  Filters threads based on keywords
 // @author       Kyle
 // @match        https://www.resetera.com/*
@@ -10,8 +10,8 @@
 // @license CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @author          Kyle Murphy
-// @downloadURL https://github.com/murphyky/reseteraScripts/blob/master/reseterafilter.user.js
-// @updateURL https://github.com/murphyky/reseteraScripts/blob/master/reseterafilter.user.js
+// @downloadURL https://github.com/murphyky/reseteraScripts/raw/master/reseterafilter.user.js
+// @updateURL https://github.com/murphyky/reseteraScripts/raw/master/reseterafilter.user.js
 
 // ==/UserScript==
 
