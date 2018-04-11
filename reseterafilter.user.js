f8759149751933d58d7f4864f5f4d90fe2c61690
kyle <kyle-murphy.co.uk>
2018-04-11 21:42:03 +0100

    include keyworld filtering input. Feature for removing filters already applied. Some styling needed

--- reseterafilter.user.js
+++ reseterafilter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Resetera filter threads
-// @version      0.76
+// @version      0.8
 // @description  Filters threads based on keywords
 // @author       Kyle Murphy
 // @match        https://www.resetera.com/*
@@ -33,21 +33,112 @@ color: white !important;
 text-align:center;
 padding:0 4px;
 }
+#blockedThreadsDropdown{
+    width: 300px;
+}
 `);
 
 (function() {
     'use strict';
+
+    var blockedThreadsDropdown = document.createElement("select");
+    var defaultOption = document.createElement("option")
+    defaultOption.value = "--- Remove a filter ---";
+    blockedThreadsDropdown.appendChild(defaultOption);
+    blockedThreadsDropdown.id = "blockedThreadsDropdown";
+
+    var topNav = document.getElementsByClassName("PageNav");
+    topNav = topNav[0];
+
+    var unblockButton = document.createElement("button");
+    unblockButton.onclick = unblockThread;
+    unblockButton.innerText = "Unblock Item";
+
+    var unblockContainer = document.createElement("div");
+    unblockContainer.className = "unblockContainer";
+    unblockContainer.appendChild(blockedThreadsDropdown);
+    unblockContainer.appendChild(unblockButton);
+
+    var keyWordFilter = document.createElement("input");
+    keyWordFilter.id = "keyWordFilter";
+    keyWordFilter.label = "Enter keyword to filter out threads";
+    keyWordFilter.type = "text";
+    var filterKeywordButton = document.createElement("button");
+    filterKeywordButton.onclick = filterByKeyword;
+    filterKeywordButton.innerText = "Add keyword to filter";
+    filterKeywordButton.id = "filterKeywordButton";
+
+    var keyworldFilterContainer = document.createElement("div");
+    keyworldFilterContainer.id = "keyworldFilterContainer";
+
+    keyworldFilterContainer.appendChild(keyWordFilter);
+    keyworldFilterContainer.appendChild(filterKeywordButton);
+
+    var extraFilteringOptionsContainer = document.createElement("div");
+
+    extraFilteringOptionsContainer.appendChild(keyworldFilterContainer);
+    extraFilteringOptionsContainer.appendChild(unblockContainer);
+
+    topNav.insertAdjacentElement("afterend", extraFilteringOptionsContainer);
+
+
+    function filterByKeyword() {
+
+        //prevent empty strings from removing every thread
+        if (!!keyWordFilter.value.trim()) {
+            pushToBlocklist(keyWordFilter.value);
+            hideShowThreads();
+        }
+    }
+
+    function initiateBlockedThreadDropdown() {
+        localStorage.blockList = localStorage.blockList || "[]";
+        var blockList = JSON.parse(localStorage.blockList);
+        for (var idx in blockList) {
+            var opt = document.createElement("option");
+            opt.value = blockList[idx];
+            opt.innerText = blockList[idx];
+            blockedThreadsDropdown.appendChild(opt);
+        }
+    }
+
+    function unblockThread() {
+        var val = blockedThreadsDropdown.value;
+        localStorage.blockList = localStorage.blockList || "[]";
+        var blockList = JSON.parse(localStorage.blockList);
+        blockList = blockList.filter(function(blockedItem) {
+            return blockedItem !== val;
+        });
+        localStorage.blockList = JSON.stringify(blockList);
+        hideShowThreads();
+
+        for (var i = 0; i < blockedThreadsDropdown.length; i++) {
+            if (val === blockedThreadsDropdown.options[i].value){
+                blockedThreadsDropdown.remove(i);
+            }
+        }
+    }
 
     window.pushToBlocklist = function (str) {
         localStorage.blockList = localStorage.blockList || "[]";
         var blockList = JSON.parse(localStorage.blockList);
         blockList.push(str);
         localStorage.blockList = JSON.stringify(blockList);
+
+        var newOption = document.createElement("option");
+        newOption.value = str;
+        newOption.innerText = str;
+        blockedThreadsDropdown.appendChild(newOption);
     };
 
-    window.onload = hideThreads;
+    window.onload = init;
 
-    function hideThreads(){
+    function init() {
+        hideShowThreads();
+        initiateBlockedThreadDropdown();
+    }
+
+    function hideShowThreads(){
 
         var blockList = localStorage.blockList ? JSON.parse(localStorage.blockList) : [];
 
@@ -61,6 +152,11 @@ padding:0 4px;
                 if (elem.innerText.toLowerCase().indexOf(blockList[i].toLowerCase())>-1){
                     console.log("Hiding this thread", elem.innerText);
                     elem.style.display = "none";
+                } else {
+                    if (elem.style.display === "none") {
+                        console.log("Unhiding this thread", elem.innerText);
+                        elem.style.display = "";
+                    }
                 }
             }
         }
@@ -75,7 +171,7 @@ padding:0 4px;
         var threadTitle = e.toElement.parentElement.previousElementSibling.getElementsByTagName("h3")[0];
         var blockThreadText = threadTitle.innerText;
         pushToBlocklist(blockThreadText);
-        hideThreads();
+        hideShowThreads();
     }
 
     for (var i = 0; i < threads.length; i++) {
